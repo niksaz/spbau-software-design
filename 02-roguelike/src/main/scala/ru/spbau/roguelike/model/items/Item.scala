eaa20dabcf985e59f7bf54a1de6dd758665a0a24
niksaz <nikitasazanovich@gmail.com>
2018-05-01 19:05:57 +0300

    model.items tested.

--- 02-roguelike/src/main/scala/ru/spbau/roguelike/model/items/Item.scala
+++ 02-roguelike/src/main/scala/ru/spbau/roguelike/model/items/Item.scala
@@ -2,16 +2,21 @@ package ru.spbau.roguelike.model.items
 
 import ru.spbau.roguelike.model.combat.CombatStats
 
+/** Trait which stands for a slot of [[ru.spbau.roguelike.model.characters.PlayerCharacter]]. */
 sealed trait CharacterItemSlot {
+  /** Returns the displayed name of the slot. */
   val name: String
 }
 
+/** A slot for character's hands. */
 case object HandsItemSlot extends CharacterItemSlot {
   override val name: String = "Hands"
 }
 
+/** A slot for character's body. */
 case object BodyItemSlot extends CharacterItemSlot {
   override val name: String = "Body"
 }
 
+/** Represents an item what can be found on the map by a player. */
 case class Item(name: String, stats: CombatStats, itemSlot: CharacterItemSlot)
